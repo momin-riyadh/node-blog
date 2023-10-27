469011db1af4458817f99da8d3f672eb92ea6ec7
momin-riyadh <mominriyadh@gmail.com>
2023-10-27 10:39:40 +0600

    fix and deploy

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require('path');
 
 const port = process.env.PORT || 8080;
 
 // INITIATE EXPRESS APP
 const app = express();
 
-app.use(express.static("public"));
+app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", "ejs");
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // if (process.env.NODE_ENV === "development") {
